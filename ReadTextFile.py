ba504aba7834e8ae5c4a4357349414206bcf2336
vikhyatsrivastava <vikhyatsrivastava2012@gmail.com>
2023-09-23 17:29:35 +0100

    Initial commit

---
+++ ReadTextFile.py
@@ -0,0 +1,10 @@
+# Author: Vikhyat Srivastava
+# Date: 23/09/2023
+
+# This program reads file written in text format and counts number of lines and number of words in each line.
+
+def readFile():
+    print('Enter the full path of the file.')
+
+if __name__ == '__main__':
+    readFile
