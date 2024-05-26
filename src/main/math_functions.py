920574538edbc4ea1b42a1db3de4990956625c8d
Vikhyat K Srivastava <64738245+vikhyatksrivastava@users.noreply.github.com>
2024-05-26 14:15:24 +0100

    Added more test cases

--- src/main/math_functions.py
+++ src/main/math_functions.py
@@ -1,5 +1,11 @@
 import math
 
 
+# Python function to calculate square of provided number
 def calculate_square_of_the_number(number):
     return math.pow(number, 2)
+
+
+# Python function to calculate square of provided number
+def calculate_square_root_of_the_number(number):
+    return math.sqrt(number)
--- src/test/test_math_functions.py
+++ src/test/test_math_functions.py
@@ -1,7 +1,7 @@
 # test_math_operations.py
 
 import unittest
-from src.main.math_functions import calculate_square_of_the_number
+from src.main.math_functions import calculate_square_of_the_number, calculate_square_root_of_the_number
 
 
 class TestMathOperations(unittest.TestCase):
@@ -15,6 +15,15 @@ class TestMathOperations(unittest.TestCase):
 
     def test_square_zero(self):
         self.assertEqual(calculate_square_of_the_number(0), 0)
+
+
+    def test_sqrt_positive_number(self):
+        self.assertEqual(calculate_square_root_of_the_number(4), 2)
+        self.assertEqual(calculate_square_root_of_the_number(225), 15)
+
+    def test_sqrt_negative_number(self):
+        #self.assertNotEquals(calculate_square_root_of_the_number(-225), 15)
+        self.assertEqual(calculate_square_root_of_the_number(0), 0)
 
 
 if __name__ == '__main__':
